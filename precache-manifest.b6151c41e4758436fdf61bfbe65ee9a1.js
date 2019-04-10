c14adb6adaff0d55c6ce951b5dc3996b6e1b03ec
phanirithvij <phanirithvij20003@gmail.com>
2019-04-10 17:21:08 +0530

    Updates

---
+++ precache-manifest.b6151c41e4758436fdf61bfbe65ee9a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6981b213064ce9063b55",
+    "url": "/moosic/static/css/main.913e2b20.chunk.css"
+  },
+  {
+    "revision": "6981b213064ce9063b55",
+    "url": "/moosic/static/js/main.efe5d3ea.chunk.js"
+  },
+  {
+    "revision": "9f5cbdd7730f54faf442",
+    "url": "/moosic/static/js/runtime~main.0b145211.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/moosic/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ccc9d21e208b3ea7785293e63b9621af",
+    "url": "/moosic/index.html"
+  }
+];
