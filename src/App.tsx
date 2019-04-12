3bfd28f218a9e4bc5e8b6286ba928c73826e8375
phanirithvij <phanirithvij20003@gmail.com>
2019-04-12 11:44:18 +0530

    updated some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { MouseEvent } from "react";
 import './App.css';
 import Player from './components/Player/Player';
 
 export interface AppProps { };
 
-class App extends Component {
-	props! : AppProps;
-	constructor(props: AppProps){
-		super(props);
+declare global{
+	interface Window { /* any modifications to window */ }
+};
 
-	}
-	componentDidMount(){
-		fetch("").then();
-	}
-	render() {
-		return (
-			<div>
-				<Player src="" srclist={[]}/>
-			</div>
-		);
-	}
+const App = (props: AppProps) => {
+	const [ ass, setAss ] = useState({ p : 0 });
+	useEffect(()=>{
+		document.title = `${ass.p}`;
+	}, [ass.p]);
+	const plus1 = (e: MouseEvent) => {
+		let start = performance.now();
+		setAss({p:ass.p+1});
+		let end = performance.now();
+		console.log(`${end-start} ms`);
+	};
+	const minus1 = (e: MouseEvent) => {
+		let start = performance.now();
+		setAss({p:ass.p-1});
+		let end = performance.now();
+		console.log(`${end-start} ms`);
+	};
+	const clear = (e: MouseEvent) => {
+		let start = performance.now();
+		setAss({p:0});
+		let end = performance.now();
+		console.log(`${end-start} ms`);
+	};
+	return (
+		<div>
+			<Player src="" srclist={[]}/>
+			<button onClick={plus1}>+1</button>
+			<button onClick={minus1}>-1</button>
+			<button onClick={clear}>clear</button>
+		</div>
+	);
 }
 
 export default App;
