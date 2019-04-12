99e843268092e252581c4601a6eded2925b787a3
phanirithvij <phanirithvij20003@gmail.com>
2019-04-12 11:58:47 +0530

    no props

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,36 @@ import Player from './components/Player/Player';
 
 export interface AppProps { };
 
-declare global{
+declare global {
 	interface Window { /* any modifications to window */ }
 };
 
-const App = (props: AppProps) => {
-	const [ ass, setAss ] = useState({ p : 0 });
-	useEffect(()=>{
+const App = (_props: AppProps) => {
+	const [ass, setAss] = useState({ p: 0 });
+	useEffect(() => {
 		document.title = `${ass.p}`;
 	}, [ass.p]);
 	const plus1 = (e: MouseEvent) => {
 		let start = performance.now();
-		setAss({p:ass.p+1});
+		setAss({ p: ass.p + 1 });
 		let end = performance.now();
-		console.log(`${end-start} ms`);
+		console.log(`${end - start} ms`);
 	};
 	const minus1 = (e: MouseEvent) => {
 		let start = performance.now();
-		setAss({p:ass.p-1});
+		setAss({ p: ass.p - 1 });
 		let end = performance.now();
-		console.log(`${end-start} ms`);
+		console.log(`${end - start} ms`);
 	};
 	const clear = (e: MouseEvent) => {
 		let start = performance.now();
-		setAss({p:0});
+		setAss({ p: 0 });
 		let end = performance.now();
-		console.log(`${end-start} ms`);
+		console.log(`${end - start} ms`);
 	};
 	return (
 		<div>
-			<Player src="" srclist={[]}/>
+			<Player src="" srclist={[]} />
 			<button onClick={plus1}>+1</button>
 			<button onClick={minus1}>-1</button>
 			<button onClick={clear}>clear</button>
